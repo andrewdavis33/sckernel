3571520bcb32a424418d6d89a64538d515bf4b5b
Andrew Davis <andrewdavis33@gmail.com>
2020-11-17 09:45:56 -0500

    Tested for race conditions

--- sclangSub.py
+++ sclangSub.py
@@ -5,86 +5,112 @@ import sys
 import signal
 import os
 from subprocess import Popen, PIPE, DEVNULL
+import subprocess
+import time
 
-# TODO: Need to make sure there are no race conditions
-# TODO: gracefully exiting if sclang not in path
-
+# Catch sigquit and sigterm?
 
 class SclangSubprocess:
-    # Constructor
-    def __init__(self, input_prompt="sc3>", reply_prompt="->", encoding="utf-8"):
-        # Signal handler for SIGINT
-        # Log when process has seen the first shutdown call
-        def sigint_handler(signum, frame):
-            self.shutdown()
-            sys.exit(0)
-        self.shuttingDown = False
-        signal.signal(signal.SIGINT, sigint_handler)
-       
-        # prompts and encoding
-        self.input_prompt = input_prompt
-        self.reply_prompt = reply_prompt
-        self.encoding = encoding
-        
-        # Queue instance variables
-        self.sclangToWindowQ = queue.Queue()
-        
-        # Subprocesses
+    def __init__(self, encoding="utf-8"):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        self.window = Popen(
-            ["python", os.path.join(dir_path, "window.py")], 
-            stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, bufsize=0
-        )
+        window_path = os.path.join(dir_path, "window.py")
+        if not os.path.exists(window_path):
+            raise("window.py is not in the same directory as" +
+            "sclangSub.py")
 
-        self.sclang = Popen(
-            ["sclang", "-i", "sckernel"], # IDE mode allows for no prompt text in stdout 
-            stdin=PIPE, stdout=PIPE, stderr=DEVNULL, bufsize=0
-        )
+        self.encoding = encoding
+        self.window_queue = queue.Queue()
 
-        # Worker thread to process sclang output and window input
-        self.receiverStdout = threading.Thread(
-            target=self.__receive_output, 
-            args=(self.sclang.stdout,)
-        )
+        try: 
+            self.window = Popen(
+                ["python", window_path], 
+                stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, bufsize=0
+            )
+        except:
+            raise RuntimeError("Could not open window.py.  Make sure"
+            + " that 'python' is in $PATH.")
+
+        try:
+            self.sclang = Popen(
+                # IDE mode suppresses sc3> prompt text and input
+                ["sclang", "-i", "sckernel"],
+                stdin=PIPE, stdout=PIPE, stderr=DEVNULL, bufsize=0
+            )
+        except:
+            self.window.kill()
+            self.window.wait()
+            raise RuntimeError("Could not open sclang.  Check to make "
+            + "sure sclang is in $PATH")
+
+        self.receiver = threading.Thread(target=self.__receive_output)
+        self.receiver.daemon = True
         self.processor = threading.Thread(target=self.__process_output)
+        self.processor.daemon = True
+        self.receiver.start()
+        self.processor.start() 
 
-        # Start threads
-        self.receiverStdout.start()
-        self.processor.start()
+        self.shuttingDown = False
+        self.lock = threading.RLock()
+        
+        def sigint_handler(signum, frame):
+            self.shutdown() 
+        
+        signal.signal(signal.SIGINT, sigint_handler)
 
-    # Private methods
-    def __receive_output(self, fd):
+    def __receive_output(self):
         while True:
-            outputLine = fd.readline() # blocks, no timeout
+            try:
+                outputLine = self.sclang.stdout.readline()
+            except:
+                break
             if not outputLine: break
             outputLine = outputLine.decode(self.encoding)
-            self.sclangToWindowQ.put(outputLine)
+            self.window_queue.put(outputLine)
         
-        # Indicate to window thread that there is no more output
-        self.sclangToWindowQ.put(None)
-        
-        # interrupt main thread which needs to catch signal.SIGINT
-        if not self.shuttingDown: os.kill(os.getpid(), signal.SIGINT)
+        # Tell window loop to stop processing
+        self.window_queue.put(None)
 
     def __sendToWindow(self, line):
-        lineWithNewline = line + '\n'
-        self.window.stdin.write(lineWithNewline.encode(self.encoding))
-
-    def __getLine(self):
-        line = self.sclangToWindowQ.get() # blocks, no timeout
-        self.sclangToWindowQ.task_done()
-        if line == None: return None
-        return line.rstrip()
+        try:
+            self.window.stdin.write(line.encode(self.encoding))
+        except:
+            # Do not quit process because we may still be
+            # able to read and write from sclang.
+            print("Cannot write to window.py. Continuing execution.")
 
     def __process_output(self):
         while True:
-            line = self.__getLine()
+            line = self.window_queue.get()
  
             if line == None: break
             else: 
-                self.__sendToWindow(line)        
-    
-    # Public methods
+                self.__sendToWindow(line)
+
+        self.window.stdin.close() 
+        
+        self.lock.acquire()
+        if not self.shuttingDown:
+            self.shuttingDown = True
+            self.lock.release()
+
+            # sclang interpreter must have terminated
+            print()
+            print("sclang interpeter shutdown. Exiting "
+            "from worker thread.")
+            self.sclang.wait()
+            self.window.wait()
+            os._exit(1)
+        else:
+            pass # let shutdown happen from main thread
+ 
+    def get_code(self):
+        try:
+            code = input("sc3> ")
+            error = self.send_code(code)
+            if error: self.shutdown()
+        except EOFError:
+            self.shutdown()
+
     def send_code(self, code, silent = False):
         if code == '': return False, code
         
@@ -93,47 +119,39 @@ class SclangSubprocess:
         # attempted to write to sclang, otherwise
         # report that sclang has been shutdown
         try:
-            self.sclang.stdin.write(code.encode())
-
+            self.sclang.stdin.write(code.encode(self.encoding))
             if silent:
                 self.sclang.stdin.write(bytearray.fromhex("1b"))
             else:
                 self.sclang.stdin.write(bytearray.fromhex("0c"))
-            return True
-        except IOError:
             return False
-
-
+        except IOError:
+            print("Could not write to stdin of sclang.")
+            return True
+        except:
+            print("Some issue other than an IOError when writing to "
+            + "stdin of sclang")
+            return True
 
     def shutdown(self):
-        self.shuttingDown = True
+        self.lock.acquire()
+        if not self.shuttingDown:
+            self.shuttingDown = True
+            self.lock.release()        
 
-        print()
-        print("Closing input to sclang stdin and waiting for" +
-        "worker thread to finish consuming sclang stdout...")
-        self.sclang.stdin.close()
-        self.receiverStdout.join()
-        print("sclang stdout consumed and consumer thread joined.")
-
-        print()
-        print("Waiting for all output to be sent to window...")
-        self.sclangToWindowQ.join()
-        self.window.stdin.close()
-        print("All output sent to window.")
-
-        print()
-        print("Waiting for window subprocess to close and" +
-        " processor thread to join...")
-        self.processor.join()
-        self.window.wait()
-        print("Window subprocess closed and processor thread joined.")
-        print("Exiting...")
-
+            print("Shutting down...")
+            print("Closing input to sclang stdin and waiting for" +
+            " subprocesses to close.")
+            self.sclang.stdin.close()
+            self.sclang.wait()
+            print("Sclang subprocess closed.")
+            self.window.wait()
+            print("Window subprocess closed.")
+            print("Exiting...")
+            sys.exit(0)
+ 
 if __name__ == "__main__":
     sclang = SclangSubprocess()
 
     while True:
-        code = input("sc3> ")
-        sclang.send_code(code)
-
-    sclang.shutdown()
+        sclang.get_code()
