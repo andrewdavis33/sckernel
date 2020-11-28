c3f794fe3fd44d70221951c1468e6870f1e7b7dc
Andrew Davis <andrewdavis33@gmail.com>
2020-11-27 19:37:45 -0500

    Added blank license file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'jupyter_client', 'IPython', 'ipykernel'
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
